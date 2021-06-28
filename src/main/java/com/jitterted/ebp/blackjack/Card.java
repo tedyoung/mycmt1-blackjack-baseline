8088e660e64a5f50009b1ae78523429e83ac6694
Ted M. Young <tedyoung@gmail.com>
2021-06-27 17:06:35 -0700

    Re-indent all source to 4-space indentation.
    Added .editorconfig with 4-space indent configuration.
    
    Updated JUnit to 5.7.2.

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -5,65 +5,65 @@ import org.fusesource.jansi.Ansi;
 import static org.fusesource.jansi.Ansi.ansi;
 
 public class Card {
-  private final String suit;
-  private final String rank;
+    private final String suit;
+    private final String rank;
 
-  public Card(String suit, String rank) {
-    this.suit = suit;
-    this.rank = rank;
-  }
-
-  public int rankValue() {
-    if ("JQK".contains(rank)) {
-      return 10;
-    } else if (rank.equals("A")) {
-      return 1;
-    } else {
-      return Integer.parseInt(rank);
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
     }
-  }
 
-  public String display() {
-    String[] lines = new String[7];
-    lines[0] = "┌─────────┐";
-    lines[1] = String.format("│%s%s       │", rank, rank.equals("10") ? "" : " ");
-    lines[2] = "│         │";
-    lines[3] = String.format("│    %s    │", suit);
-    lines[4] = "│         │";
-    lines[5] = String.format("│       %s%s│", rank.equals("10") ? "" : " ", rank);
-    lines[6] = "└─────────┘";
+    public int rankValue() {
+        if ("JQK".contains(rank)) {
+            return 10;
+        } else if (rank.equals("A")) {
+            return 1;
+        } else {
+            return Integer.parseInt(rank);
+        }
+    }
 
-    Ansi.Color cardColor = "♥♦".contains(suit) ? Ansi.Color.RED : Ansi.Color.BLACK;
-    return ansi()
-        .fg(cardColor).toString()
-        + String.join(ansi().cursorDown(1)
-                            .cursorLeft(11)
-                            .toString(), lines);
-  }
+    public String display() {
+        String[] lines = new String[7];
+        lines[0] = "┌─────────┐";
+        lines[1] = String.format("│%s%s       │", rank, rank.equals("10") ? "" : " ");
+        lines[2] = "│         │";
+        lines[3] = String.format("│    %s    │", suit);
+        lines[4] = "│         │";
+        lines[5] = String.format("│       %s%s│", rank.equals("10") ? "" : " ", rank);
+        lines[6] = "└─────────┘";
 
-  @Override
-  public String toString() {
-    return "Card {" +
-        "suit=" + suit +
-        ", rank=" + rank +
-        '}';
-  }
+        Ansi.Color cardColor = "♥♦".contains(suit) ? Ansi.Color.RED : Ansi.Color.BLACK;
+        return ansi()
+                .fg(cardColor).toString()
+                + String.join(ansi().cursorDown(1)
+                                    .cursorLeft(11)
+                                    .toString(), lines);
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public String toString() {
+        return "Card {" +
+                "suit=" + suit +
+                ", rank=" + rank +
+                '}';
+    }
 
-    Card card = (Card) o;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-    if (!suit.equals(card.suit)) return false;
-    return rank.equals(card.rank);
-  }
+        Card card = (Card) o;
 
-  @Override
-  public int hashCode() {
-    int result = suit.hashCode();
-    result = 31 * result + rank.hashCode();
-    return result;
-  }
+        if (!suit.equals(card.suit)) return false;
+        return rank.equals(card.rank);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + rank.hashCode();
+        return result;
+    }
 }
--- src/main/java/com/jitterted/ebp/blackjack/Deck.java
+++ src/main/java/com/jitterted/ebp/blackjack/Deck.java
@@ -5,24 +5,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-  private final List<Card> cards = new ArrayList<>();
+    private final List<Card> cards = new ArrayList<>();
 
-  public Deck() {
-    List<String> cardValues = List.of("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K");
-    List<String> suits = List.of("♠", "♦", "♥", "♣");
-    for (String suit : suits) {
-      for (String cardValue : cardValues) {
-        cards.add(new Card(suit, cardValue));
-      }
+    public Deck() {
+        List<String> cardValues = List.of("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K");
+        List<String> suits = List.of("♠", "♦", "♥", "♣");
+        for (String suit : suits) {
+            for (String cardValue : cardValues) {
+                cards.add(new Card(suit, cardValue));
+            }
+        }
+        Collections.shuffle(cards);
     }
-    Collections.shuffle(cards);
-  }
 
-  public int size() {
-    return cards.size();
-  }
+    public int size() {
+        return cards.size();
+    }
 
-  public Card draw() {
-    return cards.remove(0);
-  }
+    public Card draw() {
+        return cards.remove(0);
+    }
 }
